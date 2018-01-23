0af423b609b7ba893e3ad9fa2f0d715901c39807
Unknown <leietal@163.com>
2018-01-24 02:02:20 +0800

    添加注释

--- nodegit-demo.js
+++ nodegit-demo.js
@@ -5,15 +5,16 @@ var shell = require("shelljs");
 Git.Clone("https://github.com/leietal/leietal.github.io.git", "leietal")
   .then((repo) => {
     console.log("==========> clone");
-    console.log(repo);
   });
 
 // 打开
 Git.Repository.open("leietal")
   .then((repo) => {
-    console.log("==========> clone");
-    console.log(repo.workdir());
+    console.log("==========> open");
+    // 命令
     var log = `git log --since="2016-12-31" --until="2017-12-31" --no-merges --numstat --pretty=format:"%ae" > 123.txt`;
+    // 进入项目路径
     shell.cd(repo.workdir());
+    // 执行命令
     shell.exec(log, {silent: true, encoding: 'utf8'});
   });
